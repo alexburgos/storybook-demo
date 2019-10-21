c08996b29790c7c237190b4fb51c4cf44b68077c
Alex Burgos <alexander.burgos1@gmail.com>
2019-10-21 11:32:55 +0200

    dark theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,10 @@
 
 import { configure } from '@storybook/react';
 import requireContext from 'require-context.macro';
+import 'storybook-chromatic';
+import { addParameters } from '@storybook/react';
+import { themes } from '@storybook/theming';
+
 
 import '../src/index.css';
 
@@ -10,5 +14,11 @@ const req = requireContext('../src/components', true, /\.stories\.js$/);
 function loadStories() {
 	req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+	options: {
+		theme: themes.dark
+	}
+});
 
 configure(loadStories, module);
